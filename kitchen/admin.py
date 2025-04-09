ef071832c326cfe38a9f020e1be55761fc18111d
Geon-Woo <moongunwoo7019@naver.com>
2025-04-09 17:08:05 +0900

    Add action to toggle availability of Induction instances in admin panel

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import Induction, InductionTimeSlot
 
 
+@admin.action(description="사용 가능 여부 바꾸기")
+def change_is_available(modeladmin, request, inductions):
+    for induction in inductions.all():
+        induction.is_available = not induction.is_available
+        induction.save()
+
+
 @admin.register(Induction)
 class InductionAdmin(admin.ModelAdmin):
+
+    actions = [
+        change_is_available,
+    ]
+
     list_display = (
         "pk",
         "is_available",
