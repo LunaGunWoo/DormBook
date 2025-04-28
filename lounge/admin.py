5199156e7be89bfe700b9e4a059f97682cb0de34
Moon Geonwoo <moongunwoo7019@gmail.com>
2025-04-28 16:27:56 +0900

    Add PingPongTableTimeSlot model to admin interface; implement IsBookedFilter for booking status filtering and restrict adding new time slots.

--- lounge/admin.py
+++ lounge/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PingPongTable
+from django.http import HttpRequest
+from .models import PingPongTable, PingPongTableTimeSlot
 
 
 @admin.action(description="사용 가능 여부 바꾸기")
@@ -17,4 +18,37 @@ class PingPongTableAdmin(admin.ModelAdmin):
     list_display = (
         "pk",
         "is_available",
+    )
+
+
+class IsBookedFilter(admin.SimpleListFilter):
+    title = "예약됨 여부로"
+    parameter_name = "is_booked"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("true", "예"),
+            ("false", "아니오"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "true":
+            return queryset.filter(user__isnull=False)
+        if self.value() == "false":
+            return queryset.filter(user__isnull=True)
+
+
+@admin.register(PingPongTableTimeSlot)
+class PingPongTableTimeSlotAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        return False
+
+    list_filter = [
+        IsBookedFilter,
+        "ping_pong_table",
+    ]
+    readonly_fields = (
+        "start_time",
+        "end_time",
+        "booked_at",
     )
