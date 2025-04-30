1e8db9e00b2edf378663bb39e319f693cf2350d2
Moon Geonwoo <moongunwoo7019@gmail.com>
2025-04-30 12:05:15 +0900

    Add CORS support in settings.py; include corsheaders in installed apps and middleware, and configure CORS origin whitelist for local development.

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ CUSTOM_APPS = [
 THIRD_PARTY_APPS = [
     "rest_framework",
     "drf_yasg",
+    "corsheaders",
 ]
 
 SYSTEM_APPS = [
@@ -53,6 +54,7 @@ SYSTEM_APPS = [
 INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -138,3 +140,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Auth
 
 AUTH_USER_MODEL = "users.User"
+
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
